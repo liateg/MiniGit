874281e8bc4db509a5c48f24620e489e26c5da6e
ruthdg43 <ruthafruta97@gmail.com>
2025-06-19 15:35:41 +0300

    Create branch.cpp

---
+++ src/branch.cpp
@@ -0,0 +1,17 @@
+#include "../include/branch.hpp"
+#include <fstream>
+#include <iostream>
+void createBranch(const std::string& branchName) {
+    std::ifstream headFile(".minigit/HEAD");
+    std::string currentCommit;
+    std::getline(headFile, currentCommit); 
+    headFile.close();
+    std::ofstream branchFile(".minigit/branches/" + branchName);
+    if (!branchFile) {
+        std::cerr << "Failed to create branch\n";
+        return;
+    }
+    branchFile << currentCommit;
+    branchFile.close();
+    std::cout << "Branch '" << branchName << "' created.\n";
+}
