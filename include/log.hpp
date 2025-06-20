65e144a0701e3fe60b7256f2d46685ff49f303bd
ruthdg43 <ruthafruta97@gmail.com>
2025-06-20 09:06:43 +0300

    Update log.hpp

--- include/log.hpp
+++ include/log.hpp
@@ -1,27 +1,19 @@
-// #ifndef LOG_HPP
-// #define LOG_HPP
-
-// #include <string>
-
-// struct CommitNode {
-//     std::string hash;
-//     std::string parentHash;
-//     std::string message;
-//     std::string timestamp;
-//     CommitNode* next;
-// };
-
-// CommitNode* createCommitList();
-// void printCommitLog(CommitNode* head);
-// void freeCommitList(CommitNode* head);
-
-// // NEW!
-// void handleLog();
-
-// #endif
 #ifndef LOG_HPP
 #define LOG_HPP
 
+#include <string>
+
+struct CommitNode {
+    std::string hash;
+    std::string parentHash;
+    std::string message;
+    std::string timestamp;
+    CommitNode* next;
+};
+
+CommitNode* createCommitList();
+void printCommitLog(CommitNode* head);
+void freeCommitList(CommitNode* head);
 void handleLog();
 
-#endif
+#endif // LOG_HPP
